77e158ec4fc1f3d41327749c9a825391a07fd1c7
s17kf <s17kf@users.noreply.github.com>
2024-03-04 00:06:43 +0100

    Add device and location models.

--- devices/models.py
+++ devices/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Device(models.Model):
+    class DeviceTypes(models.TextChoices):
+        SWITCH = 'SW', _('Switch')
+        PWM = 'PWM', _('PWM regulator')
+
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    type = models.CharField(
+        max_length=10,
+        choices=DeviceTypes,
+    )
+    address = models.CharField(
+        "HW address",
+        max_length=100,
+    )
+
+    def __str__(self):
+        return f"{self.name}({self.type})"
