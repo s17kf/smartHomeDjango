52970ecf761eb0c80e16f9300832ce851625713a
s17kf <s17kf@users.noreply.github.com>
2024-03-05 12:21:41 +0100

    Redirect empty path to devices

--- smartHomeSite/urls.py
+++ smartHomeSite/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import redirect_view
+
 urlpatterns = [
+    path('', redirect_view, {'target': 'devices/'}),
     path('devices/', include('devices.urls')),
     path('admin/', admin.site.urls),
 ]
