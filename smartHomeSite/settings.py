92218a3c5e5b31df926825e059936bb8d7e1acde
s17kf <s17kf@users.noreply.github.com>
2024-09-02 15:35:53 +0200

    Add static root for static files in settings

--- smartHomeSite/settings.py
+++ smartHomeSite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,6 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 # Application definition
 
