0aa8556df9cbbbbcd0004c4ff9e9cd949ad54e99
s17kf <s17kf@users.noreply.github.com>
2024-09-02 22:16:11 +0200

    Use custom gpiocontrol lib

--- devices/management/commands/relays_periodic_update.py
+++ devices/management/commands/relays_periodic_update.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 from devices.models import Device, RelayPeriodicPeriod, RelayPeriodicDay
 from datetime import datetime
 import json, subprocess
+import gpiocontrol as gpio
 
 PIN_CONTROL_COMMAND = "/usr/bin/pinctrl"
 
@@ -42,15 +43,14 @@ class Command(BaseCommand):
                         active_time = True
                         activation_log = f"device: {device.name} active period: {period.begin} - {period.end}"
                         break
-            if active_time:
-                self.stdout.write(add_timestamp(activation_log))
-                command_result = subprocess.run([PIN_CONTROL_COMMAND, '-e', 'set', f'{pin_number}', 'op', 'dl'],
-                                                capture_output=True)
-            else:
-                self.stdout.write(add_timestamp(f"device: {device.name} - no active period"))
-                command_result = subprocess.run([PIN_CONTROL_COMMAND, '-e', 'set', f'{pin_number}', 'op', 'dh'],
-                                                capture_output=True)
 
-            if command_result.returncode != 0:
-                self.stdout.write(add_timestamp(f"ERROR: pin change: {command_result.stdout}"))
-        # self.stdout.write(self.style.SUCCESS('Successfully updated all devices'))
+            try:
+                if active_time:
+                    self.stdout.write(add_timestamp(activation_log))
+                    gpio.set(pin_number, gpio.State.LOW)
+                else:
+                    self.stdout.write(add_timestamp(f"device: {device.name} - no active period"))
+                    gpio.set(pin_number, gpio.State.HIGH)
+            except Exception as e:
+                self.stdout.write(add_timestamp(f"Pin change error: {str(e)}"))
+                continue
